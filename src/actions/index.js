6b5155f856466f0a43d90d36767692a9e1f07a50
Adrian <adriangunia91@gmail.com>
2017-07-14 01:07:04 +0200

    Add action for search

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,26 @@
+import axios from 'axios';
 import {
-    INCREASE_COUNTER,
-    DECREASE_COUNTER
+    GET_BUSSINES
 } from '../constants/ActionsType.js';
 
-export function increaseCounter() {
+const API_KEY = '9c60fcf251e4b24a70e886b21f05ab5b';
+const ROOT_URL = `https://api.yelp.com/v3/businesses/search?location=Wrocław`;
+
+export function fetchBussines(term, location) {
+    
+    const request = axios({
+        method:'get',
+        url: ROOT_URL,
+        headers: {'Authorization': 'XNC76F5B5lPZ9Zh6XM4PMJ50aVnGNHgdrB5rbzHtNnjLnNU42VnqaGU2EDlyzoF0K05u1_M5QBuUJ3eTwCfgJO1DB1xhnBaI-GepJ9kfAZUzbAIE723rTtJetmlTWXYx'},
+    });
+
+
+    console.log(request);
+    
     return {
-        type: INCREASE_COUNTER,
+        type: GET_BUSSINES,
     };
+    
 }
 
 export function decreaseCounter() {
--- src/components/SearchBussines/index.jsx
+++ src/components/SearchBussines/index.jsx
@@ -1,14 +1,67 @@
 import React, { Component } from 'react'
 import styles from './styles.scss';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchBussines } from '../../actions/index';
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term:'' };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.getCurrentLocation = this.getCurrentLocation.bind(this);
+        
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    getCurrentLocation(){
+            const options = {
+                enableHighAccuracy: true,
+                timeout: 5000,
+                maximumAge: 0
+            };
+
+            function success(pos) {
+                const crd = pos.coords;
+                console.log(pos);
+                console.log('Your current position is:');
+                console.log(`Latitude : ${crd.latitude}`);
+                console.log(`Longitude: ${crd.longitude}`);
+                console.log(`More or less ${crd.accuracy} meters.`);
+                return crd;
+            };
+
+            function error(err) {
+                console.warn(`ERROR(${err.code}): ${err.message}`);
+            };
+
+            navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        const location = this.getCurrentLocation();
+        this.props.fetchBussines(this.state.term, location);
+        this.setState({ term:'' });
+    }
+
     render () {
         return (
-            <form className={styles.header}>
+            <form onSubmit={this.onFormSubmit} className={`${styles.header} form-group`}>
                 <div className="row">
                     <div className="col-md-12">
                         <div className="form-group">
-                            <input className="form-control" placeholder="Search term (e.g. food, restaurants). "/>
+                            <input
+                                placeholder="Search term (e.g. food, restaurants). "
+                                className="form-control"
+                                value={this.state.term}
+                                onChange={this.onInputChange}
+                            />
                         </div>
                     </div>
                     <div className="col-md-12 text-center">
@@ -25,4 +78,8 @@ class Search extends Component {
     }
 }
 
-export default Search
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators( { fetchBussines }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(Search);
